7f451b33b4f2f32a5c31010824ccb427b187057f
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-14 19:23:56 +0500

    add views_users.py with CRUD

---
+++ task_manager/views_users.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
+
+
+class UserListView(ListView):
+    model = User
+    template_name = "users/user_list.html"
+    context_object_name = "users"
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = "users/user_form.html"
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Registration successful. Please log in.")
+        return super().form_valid(form)
+
+
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = User
+    form_class = UserChangeForm
+    template_name = "users/user_form.html"
+    success_url = reverse_lazy("user_list")
+
+    def test_func(self):
+        return self.request.user.pk == self.get_object().pk
+
+    def form_valid(self, form):
+        messages.success(self.request, "Profile updated successfully.")
+        return super().form_valid(form)
+
+
+class UserDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = User
+    template_name = "users/user_confirm_delete.html"
+    success_url = reverse_lazy("user_list")
+
+    def test_func(self):
+        return self.request.user.pk == self.get_object().pk
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "Your account has been deleted.")
+        return super().delete(request, *args, **kwargs)
