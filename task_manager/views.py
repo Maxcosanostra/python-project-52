072107ddb91d284f64766311d19e5e877567cb9d
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-14 16:13:39 +0500

    update import and change def index method to def index

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def home(request):
-    return HttpResponse("Hello, World!")
+def index(request):
+    return render(request, "index.html")
