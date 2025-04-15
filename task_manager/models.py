7c99d9cdde0cf9d7292d13a1d38656a7c64c0da4
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-15 16:23:51 +0500

    import User from django.contrib.auth.models and add new Task class

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,7 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Status(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    assigned_to = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="tasks_assigned"
+    )
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        related_name="tasks"
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name="tasks_created"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
