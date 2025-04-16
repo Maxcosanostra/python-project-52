a5d7f85f9a9c829f52518878f08f0487eaaffd00
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-17 04:24:09 +0500

    models.py: change Status class with new gettext_lazy import in order to make translation/ status_list.html: change "Status Name" to "Name"

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(
+        _("Name"),
+        max_length=100,
+        unique=True
+    )
 
     def __str__(self):
         return self.name
