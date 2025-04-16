264172157327290fd90df59af7346fa1f3277214
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-17 04:41:24 +0500

    models.py: change Label class with new gettext_lazy import in order to make translation/ label_list.html: change "Label Name" to "Name"

--- task_manager/models.py
+++ task_manager/models.py
@@ -14,7 +14,11 @@ class Status(models.Model):
         return self.name
 
 class Label(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(
+        _("Name"),  # msgid="Label Name"
+        max_length=100,
+        unique=True
+    )
 
     def __str__(self):
         return self.name
