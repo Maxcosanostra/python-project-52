c3cb4e83fcc59dc3d60c230954cbe1f629434ef9
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-18 01:56:41 +0500

    add LoginView and LogoutView

--- task_manager/urls_users.py
+++ task_manager/urls_users.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
 from .views_users import (
     UserListView,
     UserCreateView,
     UserUpdateView,
     UserDeleteView,
+    LoginView,
+    LogoutView,
 )
 
 urlpatterns = [
@@ -12,6 +13,6 @@ urlpatterns = [
     path("users/create/", UserCreateView.as_view(), name="user_create"),
     path("users/<int:pk>/update/", UserUpdateView.as_view(), name="user_update"),
     path("users/<int:pk>/delete/", UserDeleteView.as_view(), name="user_delete"),
-    path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
-    path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
