980d17bc0428f075a7a9ec2c9d5b1eada1e55614
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-18 03:16:33 +0500

    add forms.py with class CustomUserCreationForm

---
+++ task_manager/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(
+        label=_("First name"),
+        max_length=30,
+        required=False,
+    )
+    last_name = forms.CharField(
+        label=_("Last name"),
+        max_length=150,
+        required=False,
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "password1",
+            "password2",
+        )
