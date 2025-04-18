25d89bef09b684cbd0ac741218a4fb09531a7da8
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-19 04:20:37 +0500

    update views_users.py with new CustomForms

--- task_manager/views_users.py
+++ task_manager/views_users.py
@@ -1,13 +1,17 @@
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.views import LoginView as AuthLoginView, LogoutView as AuthLogoutView
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.views import (
+    LoginView as AuthLoginView,
+    LogoutView as AuthLogoutView,
+    PasswordChangeView,
+)
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
 class UserListView(ListView):
@@ -23,13 +27,16 @@ class UserCreateView(CreateView):
     success_url = reverse_lazy("login")
 
     def form_valid(self, form):
-        messages.success(self.request, _("Registration successful. Please log in."))
+        messages.success(
+            self.request,
+            _("Registration successful. Please log in."),
+        )
         return super().form_valid(form)
 
 
 class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = User
-    form_class = UserChangeForm
+    form_class = CustomUserChangeForm
     template_name = "users/user_form.html"
     success_url = reverse_lazy("user_list")
 
@@ -37,7 +44,23 @@ class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return self.request.user.pk == self.get_object().pk
 
     def form_valid(self, form):
-        messages.success(self.request, _("Profile updated successfully."))
+        messages.success(
+            self.request,
+            _("Профиль успешно обновлен"),
+        )
+        return super().form_valid(form)
+
+
+class UserPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    form_class = PasswordChangeForm
+    template_name = "users/password_change_form.html"
+    success_url = reverse_lazy("user_list")
+
+    def form_valid(self, form):
+        messages.success(
+            self.request,
+            _("Пароль успешно изменен"),
+        )
         return super().form_valid(form)
 
 
@@ -50,7 +73,10 @@ class UserDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return self.request.user.pk == self.get_object().pk
 
     def delete(self, request, *args, **kwargs):
-        messages.success(self.request, _("Your account has been deleted."))
+        messages.success(
+            self.request,
+            _("Your account has been deleted."),
+        )
         return super().delete(request, *args, **kwargs)
 
 
@@ -58,13 +84,19 @@ class LoginView(AuthLoginView):
     template_name = "users/login.html"
 
     def form_valid(self, form):
-        messages.success(self.request, _("You are logged in."))
+        messages.success(
+            self.request,
+            _("You are logged in."),
+        )
         return super().form_valid(form)
 
 
 class LogoutView(AuthLogoutView):
-    next_page = reverse_lazy('index')
+    next_page = reverse_lazy("index")
 
     def post(self, request, *args, **kwargs):
-        messages.info(request, _("You have been logged out."))
+        messages.info(
+            request,
+            _("You have been logged out."),
+        )
         return super().post(request, *args, **kwargs)
