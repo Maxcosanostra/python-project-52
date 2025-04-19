6cba359037ba1e2f871cd3b5b23e6e0b3cc96620
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-19 19:58:56 +0500

    add test function for checking rollbar performance

--- task_manager/views.py
+++ task_manager/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+# def rollbar_test(request):
+#     """
+#     Тест rollbar на вызов AttributeError
+#     """
+#     a = None
+#     a.hello()  # тут сознательная ошибка
+#     return render(request, "index.html")
