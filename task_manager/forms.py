ad1236ecf0c9b1fc6056a0be7d3fb6ad57392f20
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-23 05:01:48 +0500

    change forms.py due to playwright

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Task, Status, Label
+from .models import Task
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -49,31 +49,20 @@ class CustomUserChangeForm(forms.ModelForm):
         )
 
 class TaskForm(forms.ModelForm):
-    status = forms.ModelChoiceField(
-        queryset=Status.objects.all(),
-        required=True,
-        label=_("Статус"),
-        empty_label=_("Статус"),  # ← пустая опция с текстом "Статус"
-        widget=forms.Select(attrs={"aria-label": _("Статус")}),
-    )
-    assigned_to = forms.ModelChoiceField(
-        queryset=User.objects.all(),
-        required=False,
-        label=_("Исполнитель"),
-        empty_label=_("Исполнитель"),  # ← пустая опция с текстом "Исполнитель"
-        widget=forms.Select(attrs={"aria-label": _("Исполнитель")}),
-    )
-    labels = forms.ModelMultipleChoiceField(
-        queryset=Label.objects.all(),
-        required=False,
-        label=_("Метки"),
-        widget=forms.SelectMultiple(attrs={"aria-label": _("Метки")}),
-    )
-
     class Meta:
         model = Task
-        fields = ["name", "description", "assigned_to", "status", "labels"]
+        fields = ["name", "description", "status", "assigned_to", "labels"]
+        labels = {
+            "name":        _("Имя"),
+            "description": _("Описание"),
+            "status":      _("Статус:"),
+            "assigned_to": _("Исполнитель:"),
+            "labels":      _("Метки"),
+        }
         widgets = {
-            "name": forms.TextInput(attrs={"placeholder": _("Имя")}),
+            "name":        forms.TextInput(attrs={"placeholder": _("Имя")}),
             "description": forms.Textarea(attrs={"placeholder": _("Описание")}),
+            "status":      forms.Select(attrs={"aria-label": _("Статус")}),
+            "assigned_to": forms.Select(attrs={"aria-label": _("Исполнитель")}),
+            "labels":      forms.SelectMultiple(attrs={"aria-label": _("Метки")}),
         }
