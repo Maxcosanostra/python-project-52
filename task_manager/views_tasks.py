4db7ba4487cc4e7db8b0ee0c834ab5c9f20bf3fc
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-23 20:14:46 +0500

    update views_tasks.py

--- task_manager/views_tasks.py
+++ task_manager/views_tasks.py
@@ -61,7 +61,10 @@ class TaskDeleteView(LoginRequiredMixin, DeleteView):
         """
         task = self.get_object()
         if task.author != request.user:
-            messages.error(request, _("У вас нет прав для изменения"))
+            messages.error(
+                request,
+                _("Задачу может удалить только ее автор")
+            )
             return redirect(self.success_url)
         return super().dispatch(request, *args, **kwargs)
 
