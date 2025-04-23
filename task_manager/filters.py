3cc2581cd47000751ff79ae323cfb9fe8209dee2
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-23 06:37:29 +0500

    update filters.py

--- task_manager/filters.py
+++ task_manager/filters.py
@@ -22,6 +22,10 @@ class TaskFilter(django_filters.FilterSet):
         self.request = kwargs.get("request", None)
         super().__init__(*args, **kwargs)
 
+        self.filters["status"].label = "Статус"
+        self.filters["assigned_to"].label = "Исполнитель"
+        self.filters["labels"].label = "Метка"
+
     def filter_my_tasks(self, queryset, name, value):
         if value:
             return queryset.filter(author=self.request.user)
