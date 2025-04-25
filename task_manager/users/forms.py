951f7e4b18419d8eb59c05016ea6ebb828ee2c96
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-25 16:53:48 +0500

    code refactoring: create separate user app

---
+++ task_manager/users/forms.py
@@ -0,0 +1,69 @@
+from django import forms
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+
+# ------------------------------------------------------------------
+#  Регистрация
+# ------------------------------------------------------------------
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(label=_("First name"), max_length=30, required=False)
+    last_name  = forms.CharField(label=_("Last name"),  max_length=150, required=False)
+
+    class Meta(UserCreationForm.Meta):
+        model  = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "password1",
+            "password2",
+        )
+
+
+# ------------------------------------------------------------------
+#  Профиль + смена пароля
+# ------------------------------------------------------------------
+class CustomUserChangeForm(forms.ModelForm):
+    # личные данные
+    first_name = forms.CharField(label=_("First name"), max_length=30, required=False)
+    last_name  = forms.CharField(label=_("Last name"),  max_length=150, required=False)
+
+    # смена пароля
+    password1 = forms.CharField(
+        label=_("Пароль"),
+        widget=forms.PasswordInput,
+        required=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("Подтверждение пароля"),
+        widget=forms.PasswordInput,
+        required=False,
+        help_text=_("Для подтверждения введите, пожалуйста, пароль ещё раз."),
+    )
+
+    class Meta:
+        model  = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "password1",
+            "password2",
+        )
+
+    # --- валидация двух полей пароля ------------------------------------------
+    def clean(self):
+        cleaned = super().clean()
+        pwd1, pwd2 = cleaned.get("password1"), cleaned.get("password2")
+
+        if pwd1 or pwd2:                       # пароль меняем только если что-то ввели
+            if pwd1 != pwd2:
+                raise forms.ValidationError(_("Пароли не совпадают."))
+            password_validation.validate_password(pwd1, self.instance)
+
+        return cleaned
+
---
+++ task_manager/users/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    UserListView,
+    UserCreateView,
+    UserUpdateView,
+    UserDeleteView,
+    LoginView,
+    LogoutView,
+)
+
+urlpatterns = [
+    path("users/",               UserListView.as_view(),   name="user_list"),
+    path("users/create/",        UserCreateView.as_view(), name="user_create"),
+    path("users/<int:pk>/update/", UserUpdateView.as_view(), name="user_update"),
+    path("users/<int:pk>/delete/", UserDeleteView.as_view(), name="user_delete"),
+    path("login/",  LoginView.as_view(),  name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+]
