fbf46ba6d3607bdfcc036222381f951c6a59f72d
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-28 01:16:27 +0500

    fix .env.example and settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -18,7 +18,6 @@ from dotenv import load_dotenv
 import dj_database_url
 import rollbar
 
-from django.core.exceptions import ImproperlyConfigured
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,10 +38,7 @@ PROJECT_NAME = "hexlet-code"
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-try:
-    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-except KeyError:
-    raise ImproperlyConfigured("DJANGO_SECRET_KEY environment variable is not set")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY",)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
