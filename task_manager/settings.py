24c3191bd9a79b0b9ee02f71fb7c3acbfd3668a7
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-28 02:57:09 +0500

    final error fix: remove 'DJANGO_' prefix from 'SECRET_KEY'

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -38,9 +38,7 @@ PROJECT_NAME = "hexlet-code"
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv(
-    "DJANGO_SECRET_KEY",
-)
+SECRET_KEY = os.getenv("SECRET_KEY",)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
