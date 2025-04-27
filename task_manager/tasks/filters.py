e089df216168fc2fe901a088ea18c5fcba73983e
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-04-27 06:08:38 +0500

    update filters.py translations

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,6 +1,7 @@
 import django_filters
 from django import forms
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 from ..tasks.models import Task
 
@@ -9,11 +10,11 @@ class TaskFilter(django_filters.FilterSet):
     my_tasks = django_filters.BooleanFilter(
         method="filter_my_tasks",
         widget=forms.CheckboxInput,
-        label="Только свои задачи",
+        label=_("Only my tasks"),
     )
     assigned_to = django_filters.ModelChoiceFilter(
         queryset=User.objects.all(),
-        label="Исполнитель",
+        label=_("Executor"),
     )
 
     class Meta:
@@ -24,19 +25,16 @@ class TaskFilter(django_filters.FilterSet):
             "labels": ["exact"],
         }
 
-    # ----- мелкие доработки UI -------------------------------------------
     def __init__(self, *args, **kwargs):
         self.request = kwargs.get("request")
         super().__init__(*args, **kwargs)
 
-        self.filters["status"].label = "Статус"
-        self.filters["labels"].label = "Метка"
-        # фио вместо username
+        self.filters["status"].label = _("Status")
+        self.filters["labels"].label = _("Label")
         self.filters["assigned_to"].field.label_from_instance = (
             lambda u: u.get_full_name() or u.username
         )
 
-    # ----- «только мои» ----------------------------------------------------
     def filter_my_tasks(self, queryset, name, value):
         if value and self.request:
             return queryset.filter(author=self.request.user)
